8c4033ff3ac2d6d238d377da614263afffa5a9f4
Adrian Pionka <adrian@pionka.com>
2025-01-30 19:12:22 +0100

    add SSM enum payment state and event

---
+++ src/main/java/pl/excellentapp/spring/statemachine/domain/PaymentEvent.java
@@ -0,0 +1,7 @@
+package pl.excellentapp.spring.statemachine.domain;
+
+public enum PaymentEvent {
+
+    PRE_AUTHORIZE, PRE_AUTH_APPROVED, PRE_AUTH_DECLINED, AUTHORIZE, AUTH_APPROVED, AUTH_DECLINED
+
+}
